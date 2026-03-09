3dccfee3b880b509f6753c39b542f1340bf53f99
fzihak <foysalmzihak@gmail.com>
2026-03-10 02:15:48 +0600

    feat(audit): implement strict 0600 permission file creation

--- core/audit/local_appender.go
+++ core/audit/local_appender.go
@@ -1,9 +1,28 @@
 package audit
 
+import "os"
+
 type LocalAppender struct {
 	filePath string
+	file     *os.File
 }
 
 func NewLocalAppender(path string) *LocalAppender {
 	return &LocalAppender{filePath: path}
+}
+
+func (a *LocalAppender) Open() error {
+	f, err := os.OpenFile(a.filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
+	if err != nil {
+		return err
+	}
+	a.file = f
+	return nil
+}
+
+func (a *LocalAppender) Close() error {
+	if a.file != nil {
+		return a.file.Close()
+	}
+	return nil
 }
